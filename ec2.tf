23c45fbd755fea14827693357de2a0e71d409391
bayar <bayar@nest-33s-iMac.local>
2021-12-14 15:43:27 +0800

    terraform init

---
+++ ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+
+  user_data = <<EOF
+    #!/bin/bash
+    # Use this for your user data (script from top to bottom)
+    # install httpd (   Linux 2 version)
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+    EOF
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "ami_id" {
+    type = string
+    default = "ami-002068ed284fb165b"
+}
